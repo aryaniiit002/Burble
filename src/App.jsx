9f9f24cdd7d3ba028f84ab232ee1e46db45de950
Aryaniiit002 <aryanbindal2015@gmail.com>
2021-10-01 12:59:57 +0530

    Install dependencies and format src structure

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { ChatEngine } from 'react-chat-engine';
+
+import './App.css';
+
+export const App = () => {
+    return (
+        // <ChatEngine
+        //     height="100vh"
+        //     projectID=""
+        //     userName=''
+        //     userSecret=''
+        // />
+        <div>Burble - Chat Application</div>
+    )
+}
+
+export default App;
